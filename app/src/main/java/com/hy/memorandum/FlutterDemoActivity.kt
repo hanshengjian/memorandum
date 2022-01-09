92c172ee369afc8e484eabdf472604eb21aa89fa
指尖上的开源 <hanshengjian1989@163.com>
2022-01-09 16:32:55 +0800

    update

---
+++ app/src/main/java/com/hy/memorandum/FlutterDemoActivity.kt
@@ -0,0 +1,11 @@
+package com.hy.memorandum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlutterDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flutter_demo)
+    }
+}
