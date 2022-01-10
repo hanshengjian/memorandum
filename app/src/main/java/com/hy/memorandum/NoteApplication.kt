3a166af6543abbd8ea22396467f4e04326968869
hanshengjian <hanshengjian@hikvision.com.cn>
2022-01-10 16:28:30 +0800

    [feat] 日志打印初始化

--- app/src/main/java/com/hy/memorandum/NoteApplication.kt
+++ app/src/main/java/com/hy/memorandum/NoteApplication.kt
@@ -5,25 +5,34 @@ import cody.bus.ElegantBus
 import com.alibaba.android.arouter.launcher.ARouter
 import com.hy.common.base.BaseApp
 import com.hy.common.flutter.MemFlutterConstants
+import com.hy.utils.LogUtil
 import com.tencent.mmkv.MMKV
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.FlutterEngineCache
 import io.flutter.embedding.engine.dart.DartExecutor
-import io.flutter.embedding.engine.plugins.shim.ShimPluginRegistry
 
 
 /**
  * @Author Lenovo
  */
 class NoteApplication:BaseApp() {
+
+    companion object {
+        const val TAG = "NoteApplication"
+    }
+
     override fun onCreate() {
         super.onCreate()
+        LogUtil.i(TAG, "ARouter init start")
         ARouter.openLog()
         ARouter.init(this)
+        LogUtil.i(TAG, "ARouter init end")
         ElegantBus.setDebug(true)
+        LogUtil.i(TAG, "MMKV init start")
         val rootDir = MMKV.initialize(this)
-        println("mmkv root: $rootDir")
+        LogUtil.i(TAG, "MMKV init end")
 
+        LogUtil.i(TAG, "Flutter init start")
         try {
             val flutterEngine = FlutterEngine(this)
             flutterEngine.getNavigationChannel()
@@ -35,5 +44,6 @@ class NoteApplication:BaseApp() {
         } catch (e: Throwable) {
             e.printStackTrace()
         }
+        LogUtil.i(TAG, "Flutter init end")
     }
 }
