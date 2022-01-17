1bf0c10713a8cf548f9093cb79eff96682a094f9
hanshengjian <hanshengjian@hikvision.com.cn>
2022-01-17 12:12:57 +0800

    [feat] 优化DataApiProcessor代码

--- dataCompile/src/main/java/com/hy/datacompile/DataApiProcessor.kt
+++ dataCompile/src/main/java/com/hy/datacompile/DataApiProcessor.kt
@@ -1,16 +1,13 @@
 package com.hy.datacompile
 
 import com.google.auto.service.AutoService
-import com.squareup.kotlinpoet.*
-import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.FileSpec
 import java.util.*
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
-import javax.lang.model.element.Element
-import javax.lang.model.element.ExecutableElement
 import javax.lang.model.element.TypeElement
-import javax.lang.model.element.VariableElement
 import javax.lang.model.util.Elements
+import javax.lang.model.util.Types
 import javax.tools.Diagnostic
 
 
@@ -21,6 +18,7 @@ import javax.tools.Diagnostic
 public class DataApiProcessor:AbstractProcessor(){
     //处理Element的工具类
     private var mElementUtils: Elements? = null
+    private var mTypeUtils: Types? = null
 
     //日志信息的输出
     private var mMessager: Messager? = null
@@ -34,6 +32,7 @@ public class DataApiProcessor:AbstractProcessor(){
         }
         mElementUtils = processingEnv?.elementUtils
         mMessager = processingEnv?.messager
+        mTypeUtils = processingEnv?.getTypeUtils()
     }
 
     override fun getSupportedSourceVersion(): SourceVersion {
@@ -43,7 +42,6 @@ public class DataApiProcessor:AbstractProcessor(){
     override fun getSupportedAnnotationTypes(): MutableSet<String> {
         val types = LinkedHashSet<String>()
         types.add(DataApi::class.java.canonicalName)
-        types.add(DataMethod::class.java.canonicalName)
         return types
     }
 
@@ -60,162 +58,26 @@ public class DataApiProcessor:AbstractProcessor(){
     }
 
     fun processDataApi(roundEnv: RoundEnvironment?){
-        val elementUtils = processingEnv.elementUtils
-        val typeUtils = processingEnv.getTypeUtils()
         try {
             val classElements = roundEnv?.getElementsAnnotatedWith(DataApi::class.java)
-            val methodElements = roundEnv?.getElementsAnnotatedWith(DataMethod::class.java)
             classElements?.forEach { classElement ->
                 //生成类
-                val packageElement = elementUtils.getPackageOf(classElement)
-                //包名
-                val packageName = packageElement.qualifiedName.toString()
-                val className = "${classElement.simpleName}Repository"
-                val repositoryTypeSpecBuilder = TypeSpec.classBuilder(className)
-                methodElements?.forEach { methodElement ->
-                    //生成方法
-                    val methodName = methodElement.simpleName.toString()
-                    val enclosingName = methodElement.enclosingElement.simpleName
-                    //方法注解的类和类注解如果一致，那么说明是同一个类
-                    if (classElement.simpleName.equals(enclosingName)) {
-                        val excutableElement: ExecutableElement = methodElement as ExecutableElement
-                        //获取方法的参数列表
-                        val variableElements = excutableElement.parameters
+                val packageElement = mElementUtils!!.getPackageOf(classElement)
+                val repositoryTypeSpec = HyRepositoryTypeSpec(
+                    packageElement.qualifiedName.toString(),
+                    "${classElement.simpleName}Repository", classElement
+                )
+                repositoryTypeSpec.parse()
 
-                        var parameterSpecs = mutableListOf<HyParameter>()
-                        variableElements?.forEach { parameterElement ->
-                            parameterSpecs.add(createParameterSpec(parameterElement))
-                        }
-                        repositoryTypeSpecBuilder.addFunction(
-                            createFunSpec(
-                                methodName,
-                                excutableElement,
-                                classElement,
-                                parameterSpecs
-                            )
-                        )
-                    }
-                }
-                val fileSpec = FileSpec.builder(packageName, className)
-                    .addType(repositoryTypeSpecBuilder.build())
-                    .build()
+                val fileSpec =
+                    FileSpec.builder(repositoryTypeSpec.packageName, repositoryTypeSpec.className)
+                        .addType(repositoryTypeSpec.createTypeSpec())
+                        .build()
                 fileSpec.writeTo(mFiler)
             }
         } catch (e: Exception) {
             System.out.println("DataApiProcessor exception : " + e.message)
         }
 
-    }
-
-    /**
-     * 创建方法
-     * 最多支持三个参数
-     */
-    fun createFunSpec(
-        methodName: String,
-        excutableElement: ExecutableElement,
-        classElement: Element,
-        parameterSpecs: List<HyParameter>
-    ): FunSpec {
-        val funSpecBuild = FunSpec.builder(methodName)
-        val threadPoolManagerClass = ClassName(
-            "com.hy.common.threadpool",
-            "ThreadPoolManager"
-        )
-        //获取NoteLocalDataApi
-        val datalocalClassStr = getClassFromAnnotation(classElement)!!
-        val lastIndex = datalocalClassStr.lastIndexOf(".")
-
-        val datalocalClassName = ClassName(
-            datalocalClassStr?.substring(0, lastIndex),
-            datalocalClassStr.substring(lastIndex, datalocalClassStr.length)
-        )
-        parameterSpecs.forEach {
-            funSpecBuild.addParameter(it.parameter)
-        }
-
-        //List比较特殊，因为excutableElement.returnType获取List的类型是java类型，需要处理成Kotlin的List
-        var reponseCallParameter: TypeName
-        if (excutableElement.returnType.toString().contains("List")) {
-            val startIndex = excutableElement.returnType.toString().indexOf("<")
-            val lastIndex = excutableElement.returnType.toString().lastIndexOf(">")
-            val modelTypeStr =
-                excutableElement.returnType.toString().subSequence(startIndex + 1, lastIndex)
-
-            reponseCallParameter = ClassName("kotlin.collections", "List")
-                .parameterizedBy(Class.forName(modelTypeStr.toString()).kotlin.asClassName())
-        } else {
-            reponseCallParameter = excutableElement.returnType.asTypeName()
-        }
-        //构造reponseCall参数
-        val reponseClassName = ClassName(
-            "com.hy.common.repo",
-            "ReponseCall"
-        ).parameterizedBy(reponseCallParameter)
-        val reponseParameter = ParameterSpec.builder("reponse", reponseClassName).build()
-        funSpecBuild.addParameter(reponseParameter)
-
-        funSpecBuild.addStatement("%T.threadPool.execute{", threadPoolManagerClass)
-            .addStatement("try{")
-        if (parameterSpecs.isEmpty()) {
-            funSpecBuild.addStatement("val t = %T().%N()", datalocalClassName, methodName)
-        }
-        if (parameterSpecs.size == 1) {
-            funSpecBuild.addStatement(
-                "val t = %T().%N(%N)",
-                datalocalClassName,
-                methodName,
-                parameterSpecs[0].name
-            )
-        }
-        if (parameterSpecs.size == 2) {
-            funSpecBuild.addStatement(
-                "val t = %T().%N(%N,%N)",
-                datalocalClassName,
-                methodName,
-                parameterSpecs[0].name,
-                parameterSpecs[1].name
-            )
-        }
-        funSpecBuild.addStatement("%T.%N.post{", threadPoolManagerClass, "mainHandler")
-            .addStatement("reponse?.onResponse(t)")
-            .addStatement("}")
-            .addStatement("}catch(e:%T){", java.lang.Exception::class)
-            .addStatement("%T.%N.post{", threadPoolManagerClass, "mainHandler")
-            .addStatement("reponse?.onError(e)")
-            .addStatement("}")
-            .addStatement("}")
-            .addStatement("}")
-        return funSpecBuild.build()
-    }
-
-    /**
-     * 构造参数
-     * VariableElement：
-     */
-    fun createParameterSpec(parameterElement: VariableElement): HyParameter {
-        var parameterSpec: ParameterSpec
-        //参数类型
-        parameterSpec = ParameterSpec.builder(
-            parameterElement.simpleName.toString(),
-            parameterElement.asType().asTypeName()
-        ).build()
-        return HyParameter(parameterSpec, parameterElement.simpleName.toString())
-    }
-
-    private fun getClassFromAnnotation(key: Element): String? {
-        //获取注解的class,element.getAnnotation(annotation) 会报异常.用其他方式获取
-        val annotationMirrors = key.annotationMirrors
-        for (annotationMirror in annotationMirrors) {
-            if (DataApi::class.java.getName() == annotationMirror.annotationType.toString()) {
-                val keySet: Set<ExecutableElement> = annotationMirror.elementValues.keys
-                for (executableElement in keySet) {
-                    if (executableElement.simpleName.toString() == "local") {
-                        return annotationMirror.elementValues[executableElement]!!.value.toString()
-                    }
-                }
-            }
-        }
-        return null
     }
 }
---
+++ dataCompile/src/main/java/com/hy/datacompile/HyRepositoryTypeSpec.kt
@@ -0,0 +1,176 @@
+package com.hy.datacompile
+
+import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import javax.lang.model.element.Element
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.ExecutableElement
+import javax.lang.model.element.VariableElement
+
+/**
+ * @auther:hanshengjian
+ * @date:2022/1/17
+ *
+ */
+class HyRepositoryTypeSpec(
+    val packageName: String,
+    val className: String,
+    val classElement: Element
+) {
+    var repositoryTypeSpecBuilder: TypeSpec.Builder
+
+    init {
+        repositoryTypeSpecBuilder = TypeSpec.classBuilder(className)
+    }
+
+    val THREADPOOL_MANAGER = ClassName(
+        "com.hy.common.threadpool",
+        "ThreadPoolManager"
+    )
+    val REPONSECALL = ClassName(
+        "com.hy.common.repo",
+        "ReponseCall"
+    )
+    val PARAMETER_REPONSE: String = "reponse"
+    val MAINHANDLER: String = "mainHandler"
+
+    fun parse() {
+        val encloseElements = classElement.enclosedElements
+        encloseElements.forEach {
+            if (it.kind == ElementKind.METHOD) {
+                if (it.getAnnotation(DataMethod::class.java) != null) {
+                    val excutableElement: ExecutableElement = it as ExecutableElement
+                    //获取方法的参数列表
+                    val variableElements = excutableElement.parameters
+
+                    var parameterSpecs = mutableListOf<HyParameter>()
+                    variableElements?.forEach { parameterElement ->
+                        parameterSpecs.add(createParameterSpec(parameterElement))
+                    }
+                    val methodName = excutableElement.simpleName.toString()
+                    repositoryTypeSpecBuilder.addFunction(
+                        createFunSpec(
+                            methodName,
+                            excutableElement,
+                            classElement,
+                            parameterSpecs
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    fun createTypeSpec(): TypeSpec {
+        return repositoryTypeSpecBuilder.build()
+    }
+
+
+    /**
+     * 创建方法
+     * 最多支持三个参数
+     */
+    private fun createFunSpec(
+        methodName: String,
+        excutableElement: ExecutableElement,
+        classElement: Element,
+        parameterSpecs: List<HyParameter>
+    ): FunSpec {
+        val funSpecBuild = FunSpec.builder(methodName)
+        //获取NoteLocalDataApi
+        val datalocalClassStr = getClassFromAnnotation(classElement)!!
+        val lastIndex = datalocalClassStr.lastIndexOf(".")
+
+        val datalocalClassName = ClassName(
+            datalocalClassStr?.substring(0, lastIndex),
+            datalocalClassStr.substring(lastIndex, datalocalClassStr.length)
+        )
+        parameterSpecs.forEach {
+            funSpecBuild.addParameter(it.parameter)
+        }
+
+        //List比较特殊，因为excutableElement.returnType获取List的类型是java类型，需要处理成Kotlin的List
+        var reponseCallParameter: TypeName
+        if (excutableElement.returnType.toString().contains("List")) {
+            val startIndex = excutableElement.returnType.toString().indexOf("<")
+            val lastIndex = excutableElement.returnType.toString().lastIndexOf(">")
+            val modelTypeStr =
+                excutableElement.returnType.toString().subSequence(startIndex + 1, lastIndex)
+
+            reponseCallParameter = LIST
+                .parameterizedBy(Class.forName(modelTypeStr.toString()).kotlin.asClassName())
+        } else {
+            reponseCallParameter = excutableElement.returnType.asTypeName()
+        }
+        //构造reponseCall参数
+        val reponseClassName = REPONSECALL.parameterizedBy(reponseCallParameter)
+        val reponseParameter = ParameterSpec.builder(PARAMETER_REPONSE, reponseClassName).build()
+        funSpecBuild.addParameter(reponseParameter)
+
+        funSpecBuild.addStatement("%T.threadPool.execute{", THREADPOOL_MANAGER)
+            .addStatement("try{")
+        if (parameterSpecs.isEmpty()) {
+            funSpecBuild.addStatement("val t = %T().%N()", datalocalClassName, methodName)
+        }
+        if (parameterSpecs.size == 1) {
+            funSpecBuild.addStatement(
+                "val t = %T().%N(%N)",
+                datalocalClassName,
+                methodName,
+                parameterSpecs[0].name
+            )
+        }
+        if (parameterSpecs.size == 2) {
+            funSpecBuild.addStatement(
+                "val t = %T().%N(%N,%N)",
+                datalocalClassName,
+                methodName,
+                parameterSpecs[0].name,
+                parameterSpecs[1].name
+            )
+        }
+        funSpecBuild.addStatement("%T.%N.post{", THREADPOOL_MANAGER, MAINHANDLER)
+            .addStatement("%N?.onResponse(t)", PARAMETER_REPONSE)
+            .addStatement("}")
+            .addStatement("}catch(e:%T){", java.lang.Exception::class)
+            .addStatement("%T.%N.post{", THREADPOOL_MANAGER, MAINHANDLER)
+            .addStatement("%N?.onError(e)", PARAMETER_REPONSE)
+            .addStatement("}")
+            .addStatement("}")
+            .addStatement("}")
+        return funSpecBuild.build()
+    }
+
+
+    /**
+     * 构造参数
+     * VariableElement：
+     */
+    private fun createParameterSpec(parameterElement: VariableElement): HyParameter {
+        var parameterSpec: ParameterSpec
+        //参数类型
+        parameterSpec = ParameterSpec.builder(
+            parameterElement.simpleName.toString(),
+            parameterElement.asType().asTypeName()
+        ).build()
+        return HyParameter(parameterSpec, parameterElement.simpleName.toString())
+    }
+
+    private fun getClassFromAnnotation(key: Element): String? {
+        //获取注解的class,element.getAnnotation(annotation) 会报异常.用其他方式获取
+        val annotationMirrors = key.annotationMirrors
+        for (annotationMirror in annotationMirrors) {
+            if (DataApi::class.java.getName() == annotationMirror.annotationType.toString()) {
+                val keySet: Set<ExecutableElement> = annotationMirror.elementValues.keys
+                for (executableElement in keySet) {
+                    if (executableElement.simpleName.toString() == "local") {
+                        return annotationMirror.elementValues[executableElement]!!.value.toString()
+                    }
+                }
+            }
+        }
+        return null
+    }
+
+
+}
