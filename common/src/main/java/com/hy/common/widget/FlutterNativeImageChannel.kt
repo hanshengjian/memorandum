573f35c37b7c7fc45757d9618d3bb53d6c5d256b
hanshengjian <hanshengjian@hikvision.com.cn>
2022-03-26 14:58:31 +0800

    [feat] 优化优化

--- common/src/main/java/com/hy/common/widget/FlutterNativeImageChannel.kt
+++ common/src/main/java/com/hy/common/widget/FlutterNativeImageChannel.kt
@@ -1,71 +1,54 @@
-package com.hy.common.widget;
+package com.hy.common.widget
 
-import android.content.Context;
-
-import androidx.annotation.NonNull;
-
-import com.bumptech.glide.Glide;
-import com.hy.common.manager.ActivityManager;
-import com.hy.common.threadpool.ThreadPoolManager;
-
-import java.io.File;
-import java.util.concurrent.ExecutionException;
-
-import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.hy.common.manager.ActivityManager.Companion.peek
+import com.hy.common.threadpool.ThreadPoolManager.mainHandler
+import com.hy.common.threadpool.ThreadPoolManager.threadPool
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import java.util.concurrent.ExecutionException
 
 /**
  * @auther:hanshengjian
  * @date:2022/3/25
  */
-public class FlutterNativeImageChannel implements MethodChannel.MethodCallHandler, FlutterPlugin {
-
-    private static final String TAG = "FlutterNativeImageChannel";
-    private static final String CHANNEL_ROUTER = "com.memorandum/native_image";
-
-    private Context mContext;
-    private MethodChannel channel;
-
-    @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
-        if (call.method.equals("getNativeImage")) {
-            ThreadPoolManager.INSTANCE.getThreadPool().submit(new Runnable() {
-                @Override
-                public void run() {
-                    //获取图片
-                    String url = (String) call.arguments;
-                    //Glide 加载url，获取sd卡地址
-                    //获取当前的FlutterActivity
-                    try {
-                        File mfile = Glide.with(ActivityManager.peek()).asFile().load(url).submit().get();
-                        ThreadPoolManager.INSTANCE.getMainHandler().post(new Runnable() {
-                            @Override
-                            public void run() {
-                                result.success(mfile.getAbsolutePath());
-                            }
-                        });
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+class FlutterNativeImageChannel : MethodCallHandler, FlutterPlugin {
+    private val mContext: Context? = null
+    private var channel: MethodChannel? = null
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        if (call.method == "getNativeImage") {
+            //todo width,height
+            threadPool.submit { //获取图片
+                val url = call.arguments as String
+                //Glide 加载url，获取sd卡地址
+                //获取当前的FlutterActivity
+                try {
+                    val mfile = Glide.with(peek()).asFile().load(url).submit().get()
+                    mainHandler.post { result.success(mfile.absolutePath) }
+                } catch (e: ExecutionException) {
+                    e.printStackTrace()
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
-            });
-
+            }
         }
     }
 
-    @Override
-    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
-        channel = new MethodChannel(binding.getBinaryMessenger(), CHANNEL_ROUTER);
-        channel.setMethodCallHandler(this);
+    override fun onAttachedToEngine(binding: FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, CHANNEL_ROUTER)
+        channel!!.setMethodCallHandler(this)
     }
 
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    override fun onDetachedFromEngine(binding: FlutterPluginBinding) {}
 
+    companion object {
+        private const val TAG = "FlutterNativeImageChannel"
+        private const val CHANNEL_ROUTER = "com.memorandum/native_image"
     }
 }
